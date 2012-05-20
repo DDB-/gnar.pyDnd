f4f1142a14b0509aed04f17536b8ef134646a699
Lee D. Gauthier <lee_gauthier@hotmail.com>
2012-05-20 00:23:20 -0700

    Initial Commit

---
+++ char.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import math
+
+class Player:
+
+    stats = {
+        'str':10,
+        'dex':10,
+        'con':10,
+        'int':10,
+        'wis':10,
+        'cha':10,
+        'bab':4
+    }
+
+    saves = {
+        'fort':0,
+        'refl':0,
+        'will':0
+    }
+
+    weapons = []
+
+    def __init__( self, s = 10, d = 10, c = 20, i = 10, w = 10, C = 10 ):
+        pass
+        self.stats['str'] = s
+        self.stats['dex'] = d
+        self.stats['con'] = c
+        self.stats['int'] = i
+        self.stats['wis'] = w
+        self.stats['cha'] = C
+
+    def calcAc(self):
+        pass
+
+    def melee(self):
+        return self.calcBonus( self.stats['str'] ) + self.stats['bab']
+
+    def ranged(self):
+        return self.calcBonus( self.stats['dex'] ) + self.stats['bab']
+
+    def calcCmb(self):
+        pass
+
+    def strBonus(self):
+        pass
+
+    def calcBonus(self, x):
+        return math.ceil( (x - 10)/2 )
+
+class Weapon:
+    pass
---
+++ char_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import unittest
+import char
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.testPlayer = char.Player(s = 15, d = 18, c = 20,\
+                                      i = 10, w = 12, C = 8 )
+        self.testWeapon = char.Weapon()
+
+    def test_setup(self):
+        self.assertEqual( self.testPlayer.stats['str'], 15 )
+        self.assertEqual( self.testPlayer.stats['dex'], 18 )
+        self.assertEqual( self.testPlayer.stats['con'], 20 )
+        self.assertEqual( self.testPlayer.stats['int'], 10 )
+        self.assertEqual( self.testPlayer.stats['wis'], 12 )
+        self.assertEqual( self.testPlayer.stats['cha'], 8 )
+        self.assertEqual( self.testPlayer.stats['bab'], 4 )
+
+        self.assertIsInstance( self.testPlayer.stats, dict )
+        self.assertIsInstance( self.testPlayer.saves, dict )
+        self.assertIsInstance( self.testPlayer.weapons, list )
+
+    def test_calcBonus(self):
+        self.assertEqual( self.testPlayer.calcBonus(19), 4 )
+        self.assertEqual( self.testPlayer.calcBonus(12), 1 )
+        self.assertEqual( self.testPlayer.calcBonus(11), 0 )
+        self.assertEqual( self.testPlayer.calcBonus(10), 0 )
+        self.assertEqual( self.testPlayer.calcBonus(9), -1 )
+        self.assertEqual( self.testPlayer.calcBonus(8), -1 )
+        self.assertEqual( self.testPlayer.calcBonus(7), -2 )
+
+    def test_melee(self):
+        self.assertEqual( self.testPlayer.melee(), (4 + 2) )
+
+    def test_ranged(self):
+        self.assertEqual( self.testPlayer.ranged(), (4 + 4) )
+
+if __name__ == '__main__':
+    unittest.main()
